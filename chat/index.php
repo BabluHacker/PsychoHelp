9782995dabdcc8b95149353312a33975410bd168
mehedi <mehedi1055@gmail.com>
2017-05-04 23:46:51 +0600

    smoothing fixed

--- chat/index.php
+++ chat/index.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['username'])){
         ================================================== -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>WpF Medinova : Home</title>
+        <title>PsychoHelp : Online Couseling</title>
 
         <!-- Mobile Specific Metas
         ================================================== -->
--- profile/easyappointments/config.php
+++ profile/easyappointments/config.php
@@ -30,7 +30,7 @@ class Config {
     // GENERAL SETTINGS
     // ------------------------------------------------------------------------
 
-    const BASE_URL      = 'http://localhost/profile/easyappointments';
+    const BASE_URL      = 'http://localhost/psychohelp/profile/easyappointments';
     const LANGUAGE      = 'english';
     const DEBUG_MODE    = FALSE;
 
