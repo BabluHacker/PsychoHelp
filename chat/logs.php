b661dcd5542abe01e7e3bb40521dde52826ce3e9
mehedi <mehedi1055@gmail.com>
2017-05-07 22:19:29 +0600

    almost

--- chat/logs.php
+++ chat/logs.php
@@ -19,17 +19,23 @@ if($uname != 'Admin') {
     $result1 = mysqli_query($con, "SELECT * FROM logs WHERE (username = '$uname' AND recipient = '$reci') OR (username = '$reci' AND recipient = '$uname')  ORDER by id DESC");
 
 }
-else{
-    $result1 = mysqli_query($con, "SELECT * FROM logs ORDER by id DESC");
+else{ //admin
+    if($reci == "all"){//no user selected
+        $result1 = mysqli_query($con, "SELECT * FROM logs ORDER by id DESC");
+    }
+    else{
+        $result1 = mysqli_query($con, "SELECT * FROM logs WHERE (username = '$uname' AND recipient = '$reci') OR (username = '$reci' AND recipient = '$uname')  ORDER by id DESC");
+    }
+
 }
 
 while($result1 && $extract = mysqli_fetch_array($result1)){
     if($extract['username'] == "Admin"){
-        echo "<span class ='uname' style='color: #2b542c'>" . $extract['username'] . "</span>: <span class='msg'> " . $extract['msg'] . "</span><br>";
+        echo "<span class ='uname' style='color: rgba(186,9,20,0.95)'>" . $extract['username'] . "</span>: <span class='msg'> " . $extract['msg'] . "</span><br>";
 
     }
     else {
-        echo "<span class ='uname' style='color: #0d0d54'>" . $extract['username'] . "</span>: <span class='msg'> " . $extract['msg'] . "</span><br>";
+        echo "<span class ='uname' style='color: #087806'>" . $extract['username'] . "</span>: <span class='msg'> " . $extract['msg'] . "</span><br>";
     }
 }
 
