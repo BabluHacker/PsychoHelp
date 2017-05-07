f4ec26c6c0c61081e917c063b413729a2ee35d7b
Abida16 <abida1616@gmail.com>
2017-05-07 11:22:35 +0600

    title

--- profile/access/user_profile.php
+++ profile/access/user_profile.php
@@ -56,7 +56,7 @@ if(!isset( $_SESSION["state"])){
 
 </head>
 
-<title>logged in page</title>
+<title>User Profile</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
