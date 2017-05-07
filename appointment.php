eaf83fbd390bbe9bf4bd16cf6f900cb224d04dd5
mehedi <mehedi1055@gmail.com>
2017-05-07 15:39:11 +0600

    form mail

--- appointment.php
+++ appointment.php
@@ -6,37 +6,49 @@
  * Time: 2:32 PM
  */
 
-    $name   = "Patient Name: ". $_POST["name"]."\n";
-    $email  = "email       : ". $_POST["mail"]."\n";
-    $age    = "age         : ". $_POST["age"]."\n";
-    $phone  = "Phone No    : ". $_POST["phone"]."\n";
-    $mental = "Mental State: ". $_POST["mental1"]."\n";
-    $option = "Division    : ". $_POST["option1"]."\n";
-    $sub    = $_POST["subject"];
-    $des    = $_POST["description"];
 
-    $msg = $name.$email.$age.$phone.$mental.$option;
+$name   = "Patient Name: ". $_POST["name"]."\n";
+$email  = "email       : ". $_POST["mail"]."\n";
+$age    = "age         : ". $_POST["age"]."\n";
+$phone  = "Phone No    : ". $_POST["phone"]."\n";
+$mental = "Mental State: ". $_POST["mental1"]."\n";
+$option = "Division    : ". $_POST["option1"]."\n";
+$sub    = $_POST["subject"];
+$des    = $_POST["description"];
 
-    $msg = wordwrap($msg,70);
+$msg = $name.$email.$age.$phone.$mental.$option;
 
-    $mail = "psychohelp.cnps@gmail.com";
+$msg = wordwrap($msg,70);
 
-
-
+$mail = "psychohelp.cnps@gmail.com";
 
 
-    $headers = 'From: '.$email;
-    $return = mail($mail, $sub, $msg);
 
+//echo $email;
+require_once 'profile/access/swiftmailer/lib/swift_required.php';
 
-    if(!$return){
-        echo "<script>alert('Something Occurred Wrong. Please Retry')</script>";
-    }
-    else{
-        echo "<script>alert('Appointment Request has been sent')</script>";
-    }
+$subject = 'PsychoHelp | Appointment Request'; // Give the email a subject
+//$address = "http://csedu.cf/psychohelp/profile/access/verify.php?email=".$email;
+$body = $msg;
 
-    
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+    ->setUsername('psychohelp.cnps@gmail.com')
+    ->setPassword('psychohelp')
+    ->setEncryption('ssl');
 
+$mailer = Swift_Mailer::newInstance($transport);
+
+$message = Swift_Message::newInstance($subject)
+    ->setFrom(array($mail => 'psychohelp'))
+    ->setTo(array($email))
+    ->addCc(array($mail => 'psychohelp'))
+    ->setBody($body);
+
+$result = $mailer->send($message);
+if ($result)
+    echo "SUCCESS";
+else
+    echo "ERROR";
 
 ?>
+
