38aabcddf14c06eb707d38047c8f8790765659f1
Abida16 <abida1616@gmail.com>
2017-05-09 00:10:24 +0600

    quiz  editing ...

--- profile/mysqli_connect.php
+++ profile/mysqli_connect.php
@@ -1,20 +1,20 @@
 <?php
-/*DEFINE ('DB_USER', 'psychohelp');
+DEFINE ('DB_USER', 'psychohelp');
 DEFINE ('DB_PASSWORD', 'psychohelp');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'psychohelp');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL: ' .
-    mysqli_connect_error());*/
+    mysqli_connect_error());
 
-DEFINE ('DB_USER', 'studentweb');
+/*DEFINE ('DB_USER', 'studentweb');
 DEFINE ('DB_PASSWORD', 'bismillah');
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'psycho_help');
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL: ' .
-    mysqli_connect_error());
+    mysqli_connect_error());*/
 
 ?>
