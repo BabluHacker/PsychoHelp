6d3ff4565357d0133f197c566234257269f2df5a
Abida16 <abida1616@gmail.com>
2017-05-14 21:00:16 +0600

    ea

--- profile/access/userprofile/index.php
+++ profile/access/userprofile/index.php
@@ -57,7 +57,7 @@ if(!isset( $_SESSION["state"])){
     </style>
 
     <link href="/psychohelp/profile/css/style.css" rel="stylesheet">
-    <link id="switcher" href="/css/themes/black-theme.css" rel="stylesheet">
+
 
 </head>
 
