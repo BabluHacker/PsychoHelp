fc8c6c9d00dceff1ade484a0dd9e12cf40ba9510
Abida16 <abida1616@gmail.com>
2017-05-16 22:40:49 +0600

    profile

--- profile/index.php
+++ profile/index.php
@@ -26,9 +26,11 @@ if(isset( $_SESSION["state"])){
 
     <title>Sign-Up/Login Form</title>
 
-    <link rel="stylesheet" href="css/normalize.css">
-  
-      <link rel="stylesheet" href="css/style.css">
+     <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+
+    <link rel="stylesheet" href="css/style.css">
 
 <script type ="text/javascript" src="js/registration_login.js">
 </script>
@@ -152,7 +154,7 @@ if(isset( $_SESSION["state"])){
       </div><!-- tab-content -->
       
 </div> <!-- /form -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <script src="js/index.js"></script>
 
