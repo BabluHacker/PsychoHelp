c1ac0170092a4632fa4a44097ddeea47a51ee413
Abida16 <abida1616@gmail.com>
2017-05-16 22:43:35 +0600

    profile

--- profile/index.php
+++ profile/index.php
@@ -21,7 +21,7 @@ if(isset( $_SESSION["state"])){
 <!DOCTYPE html>
 <html >
 <head>
-  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1>
+  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
 
 
     <title>Sign-Up/Login Form</title>
