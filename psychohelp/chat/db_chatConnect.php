182834e0cfa55893e4a29896d851ff08e3e06729
mehedi <mehedi1055@gmail.com>
2017-05-17 00:29:24 +0600

    init

--- psychohelp/chat/db_chatConnect.php
+++ psychohelp/chat/db_chatConnect.php
@@ -6,11 +6,18 @@
  * Time: 9:37 PM
  */
 
+DEFINE ('DB_USER', 'sql12174464');
+DEFINE ('DB_PASSWORD', 'RpsD87teFJ');
+DEFINE ('DB_HOST', 'sql12.freemysqlhosting.net');
+DEFINE ('DB_NAME', 'sql12174464');
+
+
+/*
 DEFINE ('DB_USER', 'psychohelp');
 DEFINE ('DB_PASSWORD', 'psychohelp');
 DEFINE ('DB_HOST', 'csedu.cf/phpmyadmin');
 DEFINE ('DB_NAME', 'psychohelp');
-
+*/
 $con = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to MySQL: ' .
     mysqli_connect_error());
